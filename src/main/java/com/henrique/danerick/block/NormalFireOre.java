a9fa208381b6f1384efc661c326054fe7579290f
Henrique <hassis.moreira177@hotmail.com>
2025-05-02 20:39:15 -0300

    Normal Fire Ore properties

---
+++ src/main/java/com/henrique/danerick/block/NormalFireOre.java
@@ -0,0 +1,28 @@
+package com.henrique.danerick.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+
+public class NormalFireOre extends Block {
+    public NormalFireOre(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+
+        ItemStack currentItem = player.getMainHandItem();
+
+        if (currentItem.is(ItemTags.TOOLS)) {
+            currentItem.hurtAndBreak((int) Math.round(currentItem.getMaxDamage() * 0.25), player, player1 -> player1.broadcastBreakEvent(player1.getUsedItemHand()));
+        }
+
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+    }
+}
--- src/main/java/com/henrique/danerick/init/ModBlocks.java
+++ src/main/java/com/henrique/danerick/init/ModBlocks.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.henrique.danerick.Danerick;
 import com.henrique.danerick.block.BoostedFireOre;
+import com.henrique.danerick.block.NormalFireOre;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -16,9 +17,9 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Danerick.MODID);
 
     public static final RegistryObject<Block> NORMAL_FIRE_ORE = BLOCKS.register("normal_fire_ore",
-        () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+        () -> new NormalFireOre(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
     public static final RegistryObject<Block> NORMAL_DEEPSLATE_FIRE_ORE = BLOCKS.register("normal_deepslate_fire_ore",
-        () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+        () -> new NormalFireOre(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
     public static final RegistryObject<Block> BOOSTED_FIRE_ORE = BLOCKS.register("boosted_fire_ore",
             () -> new BoostedFireOre(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
 
