6ab4288c587b9517961f04d455cbc0c8a9b12bda
Henrique <hassis.moreira177@hotmail.com>
2025-05-02 20:29:14 -0300

    Boosted Fire Ore properties

--- src/main/java/com/henrique/danerick/block/BoostedFireOre.java
+++ src/main/java/com/henrique/danerick/block/BoostedFireOre.java
@@ -1,10 +1,14 @@
 package com.henrique.danerick.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 
 public class BoostedFireOre extends Block {
     public BoostedFireOre(Properties properties) {
@@ -16,5 +20,17 @@ public class BoostedFireOre extends Block {
         super.stepOn(level, blockPos, blockState, entity);
 
         entity.setSecondsOnFire(6);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+
+        ItemStack currentItem = player.getMainHandItem();
+
+        if (currentItem.is(ItemTags.TOOLS)) {
+            currentItem.hurtAndBreak(currentItem.getMaxDamage() - currentItem.getDamageValue(), player, player1 -> player1.broadcastBreakEvent(player1.getUsedItemHand()));
+        }
+
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
 }
